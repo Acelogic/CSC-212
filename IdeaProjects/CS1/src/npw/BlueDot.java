296124028cde111ca6be66cfb7e7acc4e345f45d
Miguel Cruz <Miguelc4600@gmail.com>
2019-02-04 16:03:52 -0500

    Lab 3 3/4 completed

--- IdeaProjects/CS1/src/npw/BlueDot.java
+++ IdeaProjects/CS1/src/npw/BlueDot.java
@@ -9,13 +9,13 @@ import javax.swing.SwingUtilities;
 import painter.SPainter;
 import shapes.SCircle;
 public class BlueDot {
-// THE SOLUTION TO THE BLUE DOT PROBLEM
-private void paintTheImage() {
-    SPainter klee = new SPainter("Blue Dot",600,600);
-    SCircle dot = new SCircle(200);
-    klee.setColor(Color.BLUE);
-    klee.paint(dot);
-}
+    // THE SOLUTION TO THE BLUE DOT PROBLEM
+    private void paintTheImage() {
+        SPainter klee = new SPainter("Blue Dot",600,600);
+        SCircle dot = new SCircle(200);
+        klee.setColor(Color.BLUE);
+        klee.paint(dot);
+    }
     // REQUIRED INFRASTRUCTURE
     public BlueDot() {
         paintTheImage();
@@ -23,7 +23,7 @@ private void paintTheImage() {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new Target();
+                new BlueDot();
             }
         });
     }
