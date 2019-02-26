14192bf6a99f322921d30d4ee2b130e25a8bd3f7
Miguel Cruz <Miguelc4600@gmail.com>
2019-02-25 22:59:13 -0500

    autocommit 3

--- autoCommit.sh
+++ autoCommit.sh
@@ -5,8 +5,7 @@ read msg
 git commit -m "${msg}"
 echo "Would you like to Force This y/n ?"
 read rsp
-if[$rsp == "y"]
-then
+if [$rsp == "y"]; then
 git push origin master -f
 else
 git push origin master
