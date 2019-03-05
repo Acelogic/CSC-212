a4f387d4c49013b54b78ee1fd39e3b0940e21ea4
Miguel Cruz <Miguelc4600@gmail.com>
2019-03-05 12:43:55 -0500

    Started Task 4

--- IdeaProjects/CS1/src/shapes/ShippingContainer.java
+++ IdeaProjects/CS1/src/shapes/ShippingContainer.java
@@ -1,11 +1,23 @@
 package shapes;
+
+import java.util.Scanner;
+
 public class ShippingContainer {
 
-
     public static void main(String[] args) {
+       //Constants
+        Scanner scanner = new Scanner(System.in);
+        double width = scanner.nextDouble();
+        double length = scanner.nextDouble();
+        double height = scanner.nextDouble();
+        double face;
+        double key;
+        double distance;
+        //Objects
+        SRectangle prismLateralSides = new SRectangle(length, width);
+        SSquare prismBase =  new SSquare(height);
 
-
-
+        key = prismLateralSides.diagonal() * height;
 
 
     }
