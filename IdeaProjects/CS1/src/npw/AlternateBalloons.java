782cccd054375c412ef9af7f816a2843ee67c6b4
Acelogic <Miguelc4600@gmail.com>
2019-03-06 14:45:28 -0500

    Lab 6 Done

--- IdeaProjects/CS1/src/npw/AlternateBalloons.java
+++ IdeaProjects/CS1/src/npw/AlternateBalloons.java
@@ -50,14 +50,37 @@ public class AlternateBalloons {
 
     private void paintOneBalloon(SPainter painter) {
         Random rgen = new Random();
-        int rn = rgen.nextInt(3);
+        Color $C9D5FF = new Color(201,213 ,255);
+        Color $E817EF = new Color(232,23 ,94);
+        Color $6FFF49 = new Color(111, 255, 73);
+        Color $53C6FF = new Color(83, 198,255);
+        Color $FF8321 = new Color(255, 131, 33);
+        Color $5500FF = new Color(85, 0,255);
+        Color $FFD5FB = new Color(255,213 , 251);
+
+
+        int rn = rgen.nextInt(6);
         if (rn == 0) {
-            painter.setColor(Color.RED);
+            painter.setColor($FF8321);
         } else if (rn == 1) {
-            painter.setColor(Color.ORANGE);
-        } else {
-            painter.setColor(Color.YELLOW);
+            painter.setColor($5500FF);
         }
+        else if (rn == 2) {
+            painter.setColor($C9D5FF);
+        }
+        else if (rn == 3) {
+            painter.setColor($E817EF);
+        }
+        else if (rn == 4) {
+            painter.setColor($6FFF49);
+        }
+        else if (rn == 5) {
+            painter.setColor($53C6FF);
+        }
+        else {
+            painter.setColor($FFD5FB);
+        }
+
         painter.move();
         int balloonRadius = 30;
         SCircle balloon = new SCircle(balloonRadius);
@@ -67,4 +90,4 @@ public class AlternateBalloons {
     }
 }
 
-// TODO: 2/20/2019  TASK 5 ( LAB 6 ) 
+// TODO: 2/20/2019  DONE
