8cc69815e44b920075adfd8a0ecab2cf6111cea0
Miguel Cruz <Miguelc4600@gmail.com>
2019-03-07 22:10:51 -0500

    Fixed Assignment 3 Task 3 (Using Circles Correctly now)

--- IdeaProjects/CS1/src/shapes/GraySpace.java
+++ IdeaProjects/CS1/src/shapes/GraySpace.java
@@ -2,39 +2,28 @@ package shapes;
 
 
 public class GraySpace {
-
-    public static double pythagoreanTheorem(double a, double b){
-        double c = Math.sqrt( Math.pow(a,2) + Math.pow(b,2));
-        return c;
-    }
-
     //Constants
     static double getDarkSquareSide = 40;
-    static double quarterOfDarkSquareSide = getDarkSquareSide * 0.25;
+    static double getGraySquareGap = 3;
 
-        // Grey Square Constants
-        static double getGraySquareGap = 3;
-        static double getAmountOfGraySquares = 2;
-            // Diagonals are calculated using the Circle Class and the gaps given (Should be within restrictions)
-            static SCircle graySquareHelper = new SCircle(quarterOfDarkSquareSide);
-            static double getGraySquareDiagonal = graySquareHelper.diameter() - getGraySquareGap;
-            static double getGraySquareSide = pythagoreanTheorem(getGraySquareDiagonal/2 ,getGraySquareDiagonal/2);
+    static double getAmountOfGraySquares = 2;
+    static double getOrangeSquareBottomGap = 4.5;
+    static double getAmountOfOrangeSquares = 1;
 
-        // Orange Square Constants
-        static double getOrangeSquareBottomGap = 4.5;
-        static double getOrangeSquareTopGap = getGraySquareDiagonal/2 + getGraySquareGap;
-        static double getAmountOfOrangeSquares = 1;
-            // Diagonals are calculated using the Circle Class and the gaps given (Should be within restrictions)
-            static SCircle orangeSquareHelper = new SCircle((getDarkSquareSide - getOrangeSquareTopGap - getOrangeSquareBottomGap)/2);
-            static double getOrangeSquareDiagonal = orangeSquareHelper.diameter();
-            static double getOrangeSquareSide = pythagoreanTheorem(getOrangeSquareDiagonal/2, getOrangeSquareDiagonal/2);
+    static double getGraySquareHelperRadius = (getDarkSquareSide - 2*getGraySquareGap)/4;
+    static SCircle graySquareHelper = new SCircle(getGraySquareHelperRadius);
+
+
+    static double getOrangeSquareTopGap = getGraySquareHelperRadius + getGraySquareGap;
+    static double getOrangeSquareHelperRadius = (getDarkSquareSide - (getOrangeSquareTopGap + getOrangeSquareBottomGap))/2;
+    static SCircle orangeSquareHelper = new SCircle((getOrangeSquareHelperRadius));
 
 public static void main(String[] args) {
 
         //Object Instantiations
                 SSquare darkSquare = new SSquare(getDarkSquareSide);
-                SSquare graySquare = new SSquare(getGraySquareSide);
-                SSquare orangeSquare = new SSquare(getOrangeSquareSide);
+                SSquare graySquare = graySquareHelper.inscribingSquare();
+                SSquare orangeSquare = orangeSquareHelper.inscribingSquare();
         //Area Calculations
                 double netDarkSquareArea = darkSquare.area();
                 double netGraySquareArea = graySquare.area() * getAmountOfGraySquares;
@@ -45,12 +34,12 @@ public static void main(String[] args) {
                 System.out.println("Dark Gray Square Area: " + netDarkSquareArea);
             System.out.println("-------------------------------------------------");
                 System.out.println("Net Gray Square Area: " + netGraySquareArea + " With: " + getAmountOfGraySquares + " Gray Squares");
-                System.out.println("Gray Square Diagonal: " + getGraySquareDiagonal);
-                System.out.println("Gray Square Side: " + getGraySquareSide);
+                System.out.println("Gray Square Diagonal: " + graySquare.diagonal());
+                System.out.println("Gray Square Side: " + graySquare.side());
             System.out.println("-------------------------------------------------");
                 System.out.println("Net Orange Square Area: " + netOrangeSquareArea + " With: " + getAmountOfOrangeSquares + " Orange Squares");
-                System.out.println("Orange Square Diagonal: " + getOrangeSquareDiagonal);
-                System.out.println("Orange Square Side: " + getOrangeSquareSide);
+                System.out.println("Orange Square Diagonal: " + orangeSquare.diagonal());
+                System.out.println("Orange Square Side: " + orangeSquare.side());
             System.out.println("-------------------------------------------------");
                 System.out.println("Dark Gray Square Area Available: " + netRemainingDarkSquareArea);
             System.out.println("-------------------------------------------------");
