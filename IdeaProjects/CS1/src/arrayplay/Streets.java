78cbea221bb4d26b7080424514d23b8491f9f466
Acelogic <Miguelc4600@gmail.com>
2019-03-14 02:41:20 -0400

    Started Lab 8

---
+++ IdeaProjects/CS1/src/arrayplay/Streets.java
@@ -0,0 +1,35 @@
+package arrayplay;
+
+import java.util.Arrays;
+
+public class Streets {
+    public static void main(String[] args) {
+        String[] streets = new String[12];
+        streets[0] = "Iberville";
+        streets[1] = "Decatur";
+        streets[2] = "Toulouse";
+        streets[3] = "Bourbon";
+        streets[4] = "Dauphine";
+        streets[5] = "Royal";
+        streets[6] = "St Ann";
+        streets[7] = "St Peter";
+        streets[8] = "Conti";
+        streets[9] = "Exchange";
+        streets[10] = "Bienville";
+        streets[11] = "Dumaine";
+
+        System.out.println("Length of Array = " + streets.length);
+        System.out.println("First Element of Array = " + streets[0]);
+        System.out.println("Last Element of Array = " + streets[11]);
+
+
+        System.out.println("\nThe initial array ...");
+        int i = 0;
+        while ( i < streets.length ) {
+            System.out.println(streets[i]);
+            i = i + 1;
+        }
+
+    }
+
+}
