a069fe9e3ebd0b4b636778c99b325055b5f3da80
Acelogic <Miguelc4600@gmail.com>
2019-03-25 14:32:05 -0400

    Started Programming Assignment 4 | Task 1 Completed

---
+++ IdeaProjects/CS1/src/npw/TextRectangles.java
@@ -0,0 +1,40 @@
+package npw;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TextRectangles {
+
+
+    public static void drawOneRow(int nrOfColumns) {
+        for (int i = 0; i < nrOfColumns; i++) {
+            System.out.print("*");
+
+        }
+
+        System.out.println("");
+    }
+
+
+    public static void drawRectangle(int nrOfRows, int nrOfColumns) {
+        int i = 1;
+        while (i <= nrOfRows) {
+            drawOneRow(nrOfColumns);
+            i = i + 1;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Run:");
+        System.out.print("Number of Rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Number of Columns: ");
+        int columns = sc.nextInt();
+        drawRectangle(rows, columns);
+
+
+    }
+}
