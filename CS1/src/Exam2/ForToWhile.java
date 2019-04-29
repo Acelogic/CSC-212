f837cc45860af295037abb103726efaf1e31e2fa
acelogic <miguelc4600@gmail.com>
2019-04-29 00:32:19 -0400

    ForToWhile Added

---
+++ CS1/src/Exam2/ForToWhile.java
@@ -0,0 +1,20 @@
+package Exam2;
+
+import card.Card;
+
+public class ForToWhile {
+    public static void main(String[] args) {
+/*        // Number 5
+
+        int i = 0;
+
+        while( i < ranks.length) {
+            if (ranks.equals(ranks[i])) {
+                return weights[i];
+            }
+            i = i+1;
+
+
+        }*/
+    }
+}
